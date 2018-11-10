909758ed1a33f4ccb65fabe5304c1e1646c306df
galileofinch <galileofinchdudi@gmail.com>
2018-11-10 12:04:21 +0800

    remove search

--- src/components/ActionsBar/ActionsBar.js
+++ src/components/ActionsBar/ActionsBar.js
@@ -3,12 +3,10 @@ import PropTypes from "prop-types";
 import injectSheet from "react-jss";
 import IconButton from "@material-ui/core/IconButton";
 
-import Link from "gatsby-link";
 import { connect } from "react-redux";
 import screenfull from "screenfull";
 
 import HomeIcon from "@material-ui/icons/Home";
-import SearchIcon from "@material-ui/icons/Search";
 import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
 import FullscreenIcon from "@material-ui/icons/Fullscreen";
 import FullscreenExitIcon from "@material-ui/icons/FullscreenExit";
@@ -138,17 +136,6 @@ class ActionsBar extends React.Component {
           {((isWideScreen && navigatorShape === "open") || navigatorPosition !== "is-aside") && (
             <CategoryFilter categories={categories} filterCategory={this.categoryFilterOnClick} />
           )}
-          <IconButton
-            aria-label="Search"
-            onClick={this.searchOnClick}
-            component={Link}
-            data-shape="closed"
-            to="/search/"
-            title="Search"
-            className={classes.button}
-          >
-            <SearchIcon className={classes.button} />
-          </IconButton>
         </div>
         <div className={classes.group}>
           {navigatorPosition === "is-aside" && <FontSetter increaseFont={this.fontSetterOnClick} />}
--- src/components/Page/PageHeader.js
+++ src/components/Page/PageHeader.js
@@ -2,8 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import injectSheet from "react-jss";
 
-import AlgoliaIcon from "!svg-react-loader!../../images/svg-icons/algolia-full.svg?name=AlgoliaIcon";
-
 const styles = theme => ({
   header: {
     margin: "0 0 3em",
@@ -41,21 +39,11 @@ const styles = theme => ({
 });
 
 const Header = props => {
-  const { classes, title, algolia } = props;
+  const { classes, title } = props;
 
   return (
     <header className={classes.header}>
       <h1 className={classes.title}>{title}</h1>
-      {algolia && (
-        <a
-          className={classes.mark}
-          href="https://www.algolia.com"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <AlgoliaIcon />
-        </a>
-      )}
     </header>
   );
 };
@@ -63,7 +51,6 @@ const Header = props => {
 Header.propTypes = {
   classes: PropTypes.object.isRequired,
   title: PropTypes.string.isRequired,
-  algolia: PropTypes.bool
 };
 
 export default injectSheet(styles)(Header);
